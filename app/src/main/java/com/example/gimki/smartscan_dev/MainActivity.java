d534165ccbdae7054600c0a071d7457893649947
Robin Gielen <robin-gielen@hotmail.com>
2018-10-17 14:38:39 +0200

    Main activity cleaned up, added mdpOublie camera call working

--- app/src/main/java/com/example/gimki/smartscan_dev/MainActivity.java
+++ app/src/main/java/com/example/gimki/smartscan_dev/MainActivity.java
@@ -2,18 +2,28 @@ package com.example.gimki.smartscan_dev;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
     Context mContext;
+    private ImageView mImageView;
+    private Bitmap mImageBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +36,18 @@ public class MainActivity extends AppCompatActivity {
         // Initialisation of the buttons
         Button logged = findViewById(R.id.mainLoggedButton);
         Button login = findViewById(R.id.mainLoginButton);
-        Button manualEntry = findViewById(R.id.mainSignUpButton);
+        Button manualEntry = findViewById(R.id.mainManualEntryButton);
         Button mdpOublie = findViewById(R.id.mainMdpOublieButton);
         Button signUp = findViewById(R.id.mainSignUpButton);
+
+        FloatingActionButton camera = findViewById(R.id.mainCameraButton);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
 
         logged.setOnClickListener(new View.OnClickListener(){
 
@@ -110,5 +129,21 @@ public class MainActivity extends AppCompatActivity {
     private void launchActivitySignUp() {
         Intent intent = new Intent(this, SignUp.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
--- app/src/main/java/com/example/gimki/smartscan_dev/manualEntry.java
+++ app/src/main/java/com/example/gimki/smartscan_dev/manualEntry.java
@@ -34,12 +34,12 @@ public class manualEntry extends AppCompatActivity {
         final TextView addresse = findViewById(R.id.manualEntryAddressFill);
         final TextView codePostal = findViewById(R.id.manualEntryPostalCodeFill);
         final TextView entreprise = findViewById(R.id.manualEntryCompanyFill);
-        */
+
 
         addContactButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                /*
+
                 String tempNom = nom.getText().toString();
                 String tempPrenom = prenom.getText().toString();
                 String tempEmail = email.getText().toString();
@@ -48,9 +48,9 @@ public class manualEntry extends AppCompatActivity {
                 String tempAddresse = addresse.getText().toString();
                 String tempCodePostal = codePostal.getText().toString();
                 String tempEntreprise = entreprise.getText().toString();
-                */
+
             }
-        });
+        });*/
     }
 
 }
