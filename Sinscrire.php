8958f0f3a4c894da1d5d390f4802e4e7d4aa4d49
fabidesca <fabiandescampe@hotmail.com>
2018-10-27 11:46:48 +0200

    Update Sinscrire.php

--- Sinscrire.php
+++ Sinscrire.php
@@ -2,7 +2,7 @@
     <head>
        <meta charset="utf-8">
 		<title>SmartScanBC</title>
-        <link rel="stylesheet" href="CSS/accueil.css" media="screen" type="text/css" />
+        <link rel="stylesheet" href="CSS/inscription.css" media="screen" type="text/css" />
 		<link rel="icon" type="image/png" href="CSS/IMG/logo.png"/>
     </head>
     <body>
@@ -10,24 +10,24 @@
             <form action="" method="POST">
                 <h1 id="Inscription">Inscription</h1>
                 
-                <label>Nom</label>
-				<input type="text" name="cli_name" placeholder="Entrer votre nom" required="required" placeholder="Please Enter Name"/><br /><br />
-				<label>Prénom</label>
-				<input type="text" name="cli_firstname" placeholder="Entrer votre prénom" required="required" placeholder="Please Enter Firstname"/><br /><br />
-				<label>Adresse</label>
-				<input type="text" name="cli_add" placeholder="Entrer votre adresse" required="required" placeholder="Please Your Address"/><br/><br />
-				<label>Ville</label>
-				<input type="text" name="cli_city" placeholder="Entrer votre ville" required="required" placeholder="Please Enter Your City"/><br/><br />
-				<label>Adresse Email</label>
-				<input type="text" name="cli_mail" placeholder="Entrer l'adresse email" required="required" placeholder="Please Your E-mail"/><br/><br />
-				<label>Pseudo</label>
-				<input type="text" name="cli_username" placeholder="Entrer le nom d'utilisateur" required="required" placeholder="Please Enter Username"/><br/><br />
-				<label>Mot de passe</label>
-				<input type="password" name="cli_password" placeholder="Entrer un mot de passe" required="required" placeholder="Please Enter Password"/><br/><br />
-				<label><b>Confirmer le mot de passe</b></label>
-                <input type="password" placeholder="Ré-entrer le mot de passe" name="passwordConf" required>
-				<input type="checkbox" name="confirmer" required>
-				<label><b>Je suis d'accord avec les conditions d'utilisations</b></label>
+                <label id="nom">Nom</label>
+				<input id="nomForm" type="text" name="cli_name" placeholder="Entrer votre nom" required="required" placeholder="Please Enter Name"/><br /><br />
+				<label id="prenom">Prénom</label>
+				<input id="prenomForm" type="text" name="cli_firstname" placeholder="Entrer votre prénom" required="required" placeholder="Please Enter Firstname"/><br /><br />
+				<label id="adresse">Adresse</label>
+				<input id ="adresseForm" type="text" name="cli_add" placeholder="Entrer votre adresse" required="required" placeholder="Please Your Address"/><br/><br />
+				<label id ="ville">Ville</label>
+				<input id="villeForm" type="text" name="cli_city" placeholder="Entrer votre ville" required="required" placeholder="Please Enter Your City"/><br/><br />
+				<label id="email">Email</label>
+				<input id="emailForm" type="text" name="cli_mail" placeholder="Entrer l'adresse email" required="required" placeholder="Please Your E-mail"/><br/><br />
+				<label id="pseudo">Pseudo</label>
+				<input id="pseudoForm" type="text" name="cli_username" placeholder="Entrer le nom d'utilisateur" required="required" placeholder="Please Enter Username"/><br/><br />
+				<label id="mdp">mdp</label>
+				<input id="mdpForm" type="password" name="cli_password" placeholder="Entrer un mot de passe" required="required" placeholder="Please Enter Password"/><br/><br />
+				<label id="conf"><b>Conf mdp</b></label>
+                <input id="confForm" type="password" placeholder="Ré-entrer le mot de passe" name="passwordConf" required>
+				<input id="checkForm" type="checkbox" name="confirmer" required>
+				<label id="check"><b>Je suis d'accord avec les conditions d'utilisations</b></label>
                 <input type="submit" id='submit' name='submit' value='Créer mon compte' >
             </form>
         </div>
