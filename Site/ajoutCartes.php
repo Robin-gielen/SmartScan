3fd8fed39a6f736b008578b7f130b0002cb5f10f
tanguybmx <32740911+tanguybmx@users.noreply.github.com>
2018-11-06 15:36:56 +0100

    ajout nouveau champs

--- Site/ajoutCartes.php
+++ Site/ajoutCartes.php
@@ -21,25 +21,32 @@
 				<form action="userHomePage.php" method="POST">
 					<h1 id="Inscription">Ajout d'une carte de visite</h1>
 					
-					<label><b>Nom de l'entreprise</b></label>
-					<input type="text" placeholder="Entrez le nom de l'entreprise" name="nomEntreprise" required>
+					<label><b>Nom de la personne</b></label>
+					<input type="text" placeholder="Entrez le nom de la personne" name="nomPersonnne" required>
+					
+					<label><b>Prénom de la personne</b></label>
+					<input type="text" placeholder="Entrez le prénnom de la personne" name="prénomPersonnne" required>
 					
 					<label><b>Adresse Email</b></label>
 					<input type="email" placeholder="Entrez l'adresse email" name="email" required>
 					
-					<br>
-	
 					<label><b>Numéro de telephone</b></label>
 					<input type="text" placeholder="Entrez le numéro de telephone" name="numero" required>
-					
-					<label><b>Site web</b></label>
-					<input type="text" placeholder="Entrez l'url du site web" name="siteWeb">
 					
 					<label><b>Adresse</b></label>
 					<input type="text" placeholder="Entrez l'adresse" name="adresse">
 					
 					<label><b>Ville</b></label><br>
-					<input type="text" placeholder="Entrez la ville" name="ville" >
+					<input type="text" placeholder="Entrez la ville" name="ville">
+					
+					<label><b>Nom de l'entreprise</b></label>
+					<input type="text" placeholder="Entrez le nom de l'entreprise" name="nomEntreprise" required>
+					
+					<label><b>Activité</b></label>
+					<input type="text" placeholder="Entrez l'activite de l'entreprise" name="actiEntreprise" required>
+					
+					<label><b>Site web</b></label>
+					<input type="text" placeholder="Entrez l'url du site web" name="siteWeb">
 					<!--
 					<label><b>Lieu</b></label>
 					<input type="text" placeholder="Entrer votre lieu d'habitation" name="lieu" required>
