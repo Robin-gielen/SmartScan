f146ffc1ebf051f044fd212eeb3f0c9e0f6b21e7
fabidesca <fabiandescampe@hotmail.com>
2018-11-06 10:32:44 +0100

    ajout des liens sur les images
    ajout des liens sur les images

--- Site/Sinscrire.php
+++ Site/Sinscrire.php
@@ -1,7 +1,7 @@
 ﻿<html>
     <head>
        <meta charset="utf-8">
-		<title>SmartScanBC</title>
+		<title>Inscription</title>
         <link rel="stylesheet" href="CSS/inscription.css" media="screen" type="text/css" />
 		<link rel="icon" type="image/png" href="CSS/IMG/logo.png"/>
     </head>
