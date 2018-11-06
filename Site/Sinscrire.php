2c93990cefe385b1b55340c2a749ba6e9d1b338f
Adelin Gauthier <a2l195@hotmail.com>
2018-11-06 18:32:55 +0100

    Inscription style

--- Site/Sinscrire.php
+++ Site/Sinscrire.php
@@ -15,22 +15,42 @@
                 <h1 id="Inscription">Inscription</h1>
                 
                 <label id="nom">Nom</label>
-				<input id="nomForm" type="text" name="cli_name" placeholder="Entrez votre nom" required="required" placeholder="Please Enter Name"/><br /><br />
-				<label id="prenom">Prénom</label>
-				<input id="prenomForm" type="text" name="cli_firstname" placeholder="Entrez votre prénom" required="required" placeholder="Please Enter Firstname"/><br /><br />
+				<input id="nomform" type="text" name="cli_name" placeholder="Entrez votre nom" required="required" placeholder="Please Enter Name">
+				
+				<div id="prenom">
+				<label>Prénom</label>
+				<input id="prenomform" type="text" name="cli_firstname" placeholder="Entrez votre prénom" required="required" placeholder="Please Enter Firstname">
+				</div id="prenom">
+				<p></p>
+				
 				<label id="adresse">Adresse</label>
-				<input id ="adresseForm" type="text" name="cli_add" placeholder="Entrez votre adresse" required="required" placeholder="Please Your Address"/><br/><br />
-				<label id ="ville">Ville</label>
-				<input id="villeForm" type="text" name="cli_city" placeholder="Entrez votre ville" required="required" placeholder="Please Enter Your City"/><br/><br />
-				<label id="email">Email</label>
-				<input id="emailForm" type="text" name="cli_mail" placeholder="Entrez l'adresse email" required="required" placeholder="Please Your E-mail"/><br/><br />
-				<label id="pseudo">Pseudo</label>
-				<input id="pseudoForm" type="text" name="cli_username" placeholder="Entrez le nom d'utilisateur" required="required" placeholder="Please Enter Username"/><br/><br />
-				<label id="mdp">mdp</label>
-				<input id="mdpForm" type="password" name="cli_password" placeholder="Entrez un mot de passe" required="required" placeholder="Please Enter Password"/><br/><br />
-				<label id="conf"><b>Conf mdp</b></label>
-                <input id="confForm" type="password" placeholder="Ré-entrez le mot de passe" name="passwordConf" required>
-				<input id="checkForm" type="checkbox" name="confirmer" required>
+				<input id="adresseform" type="text" name="cli_add" placeholder="Entrez votre adresse" required="required" placeholder="Please Your Address">
+				
+				<div id="ville">
+				<label>Ville</label>
+				<input id ="villeform" type="text" name="cli_city" placeholder="Entrez votre ville" required="required" placeholder="Please Enter Your City">
+				</div>
+				<p></p>
+				
+				<label>Email</label>
+				<input id="emailform" type="text" name="cli_mail" placeholder="Entrez l'adresse email" required="required" placeholder="Please Your E-mail">
+				
+				<div id="pseudo">
+				<label>Pseudo</label>
+				<input id="pseudoform" type="text" name="cli_username" placeholder="Entrez le nom d'utilisateur" required="required" placeholder="Please Enter Username">
+				</div>
+				<p></p>
+				
+				<label>mdp</label>
+				<input id="mdpform" type="password" name="cli_password" placeholder="Entrez un mot de passe" required="required" placeholder="Please Enter Password">
+				
+				<div id="conf">
+				<label>Conf mdp</label>
+                <input id="confform" type="password" placeholder="Ré-entrez le mot de passe" name="passwordConf" required>
+				</div>
+				<p></p>
+				
+				<input type="checkbox" name="confirmer" required>
 				<label id="check"><b>Je suis d'accord avec les conditions d'utilisation</b></label>
                 <input type="submit" id='submit' name='submit' value='Créer mon compte' >
             </form>
