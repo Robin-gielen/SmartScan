4ec53ae4359531cf83dd92f2d6a7ef86dd91ae21
fabidesca <fabiandescampe@hotmail.com>
2018-11-07 10:44:07 +0100

    modification site
    modification site

---
+++ Site/userHomePage.php
@@ -0,0 +1,40 @@
+﻿<!DOCTYPE php>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>SmartScanBC</title>
+		<link rel="stylesheet" href="CSS/userHomePage.css">
+		<link rel="icon" type="image/png" href="CSS/IMG/logo.png"/>
+	</head>
+	<body>
+		<div id="header">
+			<nav>
+				<a href="index.php"><img id="smartscanbc" src="CSS/IMG/smartscanbc.png" alt="SmartScanBC"></a>
+			</nav>	
+			<nav>
+				<a href="index.php"><img id="logo" src="CSS/IMG/logo.png" alt="SmartScanBC"></a>
+			</nav>
+			<nav>
+				<div id="compte">
+					<a href="compte.php">Mon compte</a>
+				</div>
+			</nav>
+			<nav>
+				<div id="deco">
+					<a">Déconnexion</a>
+				</div>
+			</nav>
+			<nav>
+				<div id="ajoutCarte">
+					<a href="ajoutCartes.php">Ajouter une carte</a>
+				</div>
+			</nav>
+			<nav id="groupe">
+			</nav>
+			<nav id="cartes">
+			</nav>
+			<nav id="tri">
+			</nav>
+		</div>
+</body>
+</php>
