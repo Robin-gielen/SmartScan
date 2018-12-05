807cdf941793013d206b90f7f7997ed363bd0b79
fabidesca <fabiandescampe@hotmail.com>
2018-12-05 14:58:46 +0100

    modif page faq
    modif page faq

--- Site/faq.php
+++ Site/faq.php
@@ -6,20 +6,33 @@
 <html lang="fr">
 	<head>
 		<meta charset="utf-8">
-		<title>Faq</title>
+		<title>Foire aux questions</title>
 		<link rel="stylesheet" href="CSS/condGen.css">
 		<link rel="icon" type="image/png" href="CSS/IMG/logo.png"/>
 	</head>
 	<body>
 		<div>
-				<a href="index.php"><img id="smartscanbc" src="CSS/IMG/smartscanbc.png" alt="SmartScanBC"></a>
-				<a href="index.php"><img id="logoCond" src="CSS/IMG/logo.png" alt="SmartScanBC"></a>
+				<a href="index.html"><img id="smartscanbc" src="CSS/IMG/smartscanbc.png" alt="SmartScanBC"></a>
+				<a href="index.html"><img id="logoCond" src="CSS/IMG/logo.png" alt="SmartScanBC"></a>
 		</div>
 		<div>
-			<nav id="condGen">
-				<h2>Foire aux questions</h2>
-				
-			</nav>
+			<div id="titreFaq">
+				<h1>Foire aux questions</h2>
+			</div>
+			<div id="faq">
+				<h2><u>Comment s'inscrire?</u></h2>
+				<p>Vous pouvez vous inscrire via ce lien : <a href="http://www.smartscan-bc.ovh/Sinscrire.php">ici</a>.</p>
+				<h2><u>Comment se connecter?</u></h3>
+				<p>Une fois que vous vous etes inscrits, vous pouvez vous connecter via ce lien : <a href="http://www.smartscan-bc.ovh/SeConnecter.php">ici</a>.</p>
+				<h2><u>Comment se deconnecter?</u></h3>
+				<p>Si vous etes connecte, il y a un petit rectangle qui se situe en haut a droite de votre ecran afin de vous deconnecter.</p>
+				<h2><u>Comment ajouter une carte?</u></h2>
+				<p>Vous pouvez ajouter vos cartes manuellement via ce lien : <a href="http://www.smartscan-bc.ovh/ajoutCartes.php">ici</a>.</p>
+				<h2><u>Comment supprimer son compte?</u></h2>
+				<p>Il vous suffit de vous rendre dans l'onglet <i>"modifier mon compte"</i> et la vous aurez l'option <i>"supprimer mon compte"</i>.</p>
+				<h2><u>Comment nous contacter sur les reseaux sociaux?</u></h2>
+				<p>Nous sommes sur Twitter et sur Facebook. Vous pouvez rejoindre notre page Facebook <a href="https://www.facebook.com/SmartScanBC/">ici</a> et notre page Twitter <a href="https://twitter.com/SmartScanBC">ici</a>.</p>
+			</div>
 		</div>
 </body>
 </html>
