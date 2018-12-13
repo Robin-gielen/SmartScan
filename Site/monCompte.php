2799ab6f62ae5d644cebda18ee001d7721b50070
tanguybmx <32740911+tanguybmx@users.noreply.github.com>
2018-12-13 16:31:37 +0100

    modif compte

--- Site/monCompte.php
+++ Site/monCompte.php
@@ -31,6 +31,77 @@
 			session_destroy();
 			header("Location: http://www.smartscan-bc.ovh");
 	}}
+	
+	
+		if(isset($_SESSION['pseudo'])) {
+				$servername = "172.17.0.4:3306";
+				$username = "admin";
+				$password = "abc123";
+				$dbname = "smartscan";
+				$idUser = $_SESSION['id_Utilisateur'];
+				// Create connection
+				$conn = new mysqli($servername, $username, $password, $dbname);
+				
+				// Check connection
+				if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+				}
+	if(isset($_POST['conf1']) and $_POST['username']!=''){
+		
+			$username =$_POST['username'];
+			$conn->query("UPDATE Utilisateurs SET pseudo = '".$username."' WHERE id_Utilisateur=".$idUser)or die($mysqli->error());
+			$_SESSION['pseudo']=$username;
+			header("Location: http://www.smartscan-bc.ovh/compte.php");
+		
+	}
+	if(isset($_POST['conf2'])){
+		if($_POST['nom']!=''){
+		$nom = $_POST['nom'];
+		$conn->query("UPDATE Utilisateurs SET Nom = '".$nom."' WHERE id_Utilisateur=".$idUser."")or die($mysqli->error());
+		$_SESSION['nom']=$nom;
+		header("Location: http://www.smartscan-bc.ovh/compte.php");
+		}
+	}
+	if(isset($_POST['conf3'])){
+		if($_POST['prenom']!=''){
+			$prenom = $_POST['prenom'];
+			$conn->query("UPDATE Utilisateurs SET Prenom = '".$prenom."' WHERE id_Utilisateur=".$idUser."")or die($mysqli->error());
+			$_SESSION['prenom']=$prenom;
+			header("Location: http://www.smartscan-bc.ovh/compte.php");
+		}
+	}
+	if(isset($_POST['conf4'])){
+		if($_POST['email']!=''){
+			$mail = $_POST['email'];
+			$conn->query("UPDATE Utilisateurs SET Mail = '".$mail."' WHERE id_Utilisateur=".$idUser."")or die($mysqli->error());
+			$_SESSION['mail']=$mail;
+			header("Location: http://www.smartscan-bc.ovh/compte.php");
+		}
+	}
+	if(isset($_POST['conf5'])){
+		if($_POST['localite']!=''){
+			$loca = $_POST['localite'];
+			$conn->query("UPDATE Utilisateurs SET Localite = '".$loca."' WHERE id_Utilisateur=".$idUser."")or die($mysqli->error());
+			$_SESSION['localite']= $loca;
+			header("Location: http://www.smartscan-bc.ovh/compte.php");
+		}
+	}
+	if(isset($_POST['conf6'])){
+		if($_POST['password']!=''){
+				if($_POST['password']==$_POST['passwordConf']){
+					$mdp = $_POST['password'];
+			$conn->query("UPDATE Utilisateurs SET password = '".$mdp."' WHERE id_Utilisateur=".$idUser."")or die($mysqli->error());
+			header("Location: http://www.smartscan-bc.ovh/compte.php");
+				}
+				else{
+					echo " les mots de passes ne sont pas identiques";
+				}
+		}
+		}
+		}
+
+	
+	
 			
 ?>
 <!DOCTYPE html>
@@ -54,37 +125,45 @@
 				<a href='?hello=true'>Déconnexion</a>
 			</div>
 			<nav id="compte">
-				<form action="userHomePage.php" method="POST">
+				
 					<h1 id="Inscription">Modification de votre profil</h1>
-					
-					<label><b>Nom d'utilisateur</b></label>
-					<input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
-					
-					<label><b>Adresse Email</b></label>
-					<input type="text" placeholder="Entrer l'adresse email" name="email" required>
-	
+					<form  method="POST">
+					<label><b>Pseudo</b></label>
+					<input type="text" placeholder="Modifié votre pseudo" name="username" required>
+					<input type="submit" id='submit' value='Enregistrer'  name ='conf1'>
+					</form>
+					<form  method="POST">
+					<label><b>Nom</b></label>
+					<input type="text" placeholder="Modifié votre nouveau nom" name="nom" required>
+					<input type="submit" id='submit' value='Enregistrer'  name ='conf2'>
+					</form>
+					<form  method="POST">
+					<label><b>Prénom</b></label>
+					<input type="text" placeholder="Modifié votre prénom" name="prenom" required>
+					<input type="submit" id='submit' value='Enregistrer'  name ='conf3'>
+					</form>
+					<form  method="POST">
+					<label><b>Adresse email</b></label>
+					<input type="text" placeholder="Modifié votre adresse email" name="email" required>
+					<input type="submit" id='submit' value='Enregistrer'  name ='conf4'>
+					</form>
+					<form  method="POST">
+					<label><b>Localité</b></label>
+					<input type="text" placeholder="Modifié votre localité" name="localite" required>
+					<input type="submit" id='submit' value='Enregistrer'  name ='conf5'>
+					</form>
+					<form  method="POST">
 					<label><b>Mot de passe</b></label>
-					<input type="password" placeholder="Entrer le mot de passe" name="password" required>
+					<input type="password" placeholder="Modifié votre mot de passe" name="password" required>
 					
 					<label><b>Confirmer le mot de passe</b></label>
-					<input type="password" placeholder="Ré-entrer le mot de passe" name="passwordConf" required>
+					<input type="password" placeholder="Ré-entré le mot de passe" name="passwordConf" required>
+					<input type="submit" id='submit' value='Enregistrer'  name ='conf6'>
+					</form>
+
 					
-					<label><b>Sexe</b></label>
-					<input type="text" placeholder="Entrer le sexe" name="sexe" required>
 					
-					<label><b>Date</b></label><br>
-					<input type="date" placeholder="Entrer le nom d'utilisateur" name="date" required>
-					
-					<br>
-					
-					<label><b>Lieu</b></label>
-					<input type="text" placeholder="Entrer votre lieu d'habitation" name="lieu" required>
-					
-					<label><b>Entreprise</b></label>
-					<input type="text" placeholder="Entrer le nom de votre entreprise" name="entreprise" required>
-					
-					<input type="submit" id='submit' value='Enregistrer' >
-				</form>
+				
 			</nav>
 		</div>
 	</body>
