d3d4ed4e77bb1a5d3e7744acb1a339a6ff23d2be
tanguybmx <32740911+tanguybmx@users.noreply.github.com>
2018-12-13 12:49:57 +0100

    search

--- Site/userHomePage.php
+++ Site/userHomePage.php
@@ -50,7 +50,7 @@ if(isset($_SESSION['pseudo'])) {
 								}
 							}
 								if(isset($_POST['conf'])){
-									if($_POST['tri']=='alpha'){
+									if($_POST['tri']=='alpha' and !$_POST['q']){
 									$result2 =$conn2->query("select * from Contacts where id_Utilisateur ='$id_Utilisateur' order by NomSociete")or die($mysqli->error());
 										if( $result2->num_rows == 0){
 									$_SESSION['message']= "You don't have save any BC";
@@ -141,7 +141,7 @@ if(isset($_SESSION['pseudo'])) {
 	</body>
 </html>";
 									}
-									if($_POST['tri']=='chrono'){
+									if($_POST['tri']=='chrono' and !$_POST['q']){
 									$result2 =$conn2->query("select * from Contacts where id_Utilisateur ='$id_Utilisateur' order by id_Contact")or die($mysqli->error());
 										if( $result->num_rows == 0){
 									$_SESSION['message']= "You don't have save any BC";
@@ -271,7 +271,101 @@ if(isset($_SESSION['pseudo'])) {
 							
 							
 				}
-				if(!isset($_POST['conf'])){
+				
+			if($_POST['q']!='' and isset($_POST['conf'])){	
+						
+					$search = $_POST['q'];
+					$servername = "172.17.0.4:3306";
+					$username = "show";
+					$password = "vue123";
+					$dbname = "smartscan";
+					$id =$_SESSION['id_Utilisateur'];
+					$conn7 = new mysqli($servername, $username, $password, $dbname);
+					$result7 = $conn7->query("SELECT Nom, Prenom,Mail, Telephone, Adresse, Localite, NomSociete, Activite, SiteWeb  from Contacts WHERE id_Utilisateur ='.$id.' and CONCAT(Nom, Prenom,Mail, Telephone, Adresse, Localite, NomSociete, Activite, SiteWeb, Cat) LIKE '%".$search."%'")or die($mysqli->error());
+					if( $result7->num_rows != 0){
+				$cardsSearch = mysqli_fetch_all($result7,MYSQLI_ASSOC);
+				$strSearch = "<table><tr><th>Nom</th><th>Prenom</th><th>Mail</th><th>Telephone</th><th>Adresse</th><th>Localite</th><th>Nom Société</th><th>Activite</th><th>Site web</th></tr>";
+										if($cardsSearch) {
+											  foreach($cardsSearch as $val) {
+											    $strSearch .= "<td style='font-size: 1.2vw;'>" . $val['Nom'] . "</td>";
+											    $strSearch .= "<td style='font-size: 1.2vw;'>" . $val['Prenom'] . "</td>";
+												$strSearch .= "<td style='font-size: 1.2vw;'>" . $val['Mail'] . "</td>";
+											    $strSearch .= "<td style='font-size: 1.2vw;'>" . $val['Telephone'] . "</td>";
+												$strSearch .= "<td style='font-size: 1.2vw;'>" . $val['Adresse'] . "</td>";
+											    $strSearch .= "<td style='font-size: 1.2vw;'>" . $val['Localite'] . "</td>";
+												$strSearch .= "<td style='font-size: 1.2vw;'>" . $val['NomSociete'] . "</td>";
+											    $strSearch .= "<td style='font-size: 1.2vw;'>" . $val['Activite'] . "</td>";
+												$strSearch .= "<td style='font-size: 1.2vw;'>" . $val['SiteWeb'] . "</td>";
+											    $strSearch .= "</tr>";
+											    
+	 										 }
+										}
+											$strSearch.= "</table>";
+					
+					echo"<!DOCTYPE html>
+	<html lang='fr'>
+		<head>
+			<meta charset='utf-8'>
+			<title>Mes cartes</title>
+			<link rel='stylesheet' href='CSS/userHomePage.css'>
+			<link rel='icon' type='image/png' href='CSS/IMG/logo.png'/>
+		</head>
+		<body>
+			<div id='header'>
+				<a href='index.php'><img id='logo' src='CSS/IMG/logo.png' alt='SmartScanBC'></a>
+				<a href='index.php'><img id='smartscanbc' src='CSS/IMG/smartscanbc.png' alt='SmartScanBC'></a>
+				<div id='compte'>
+					<a href='compte.php'>Mon compte</a>
+				</div>
+				<div id='deco'>
+					<a href='?hello=true'>Déconnexion</a>
+				</div>
+			</div>
+			
+			
+			<div id='content'>
+			
+				<div id='left'>
+					<nav id='groupe'>
+						<img id='addGroup' src='CSS/IMG/ajouter4.png' alt='SmartScanBC'>
+						$categories
+					</nav>
+				</div>
+				
+				
+				<div id='center'>
+					<div id='tri'>
+						<form method = 'POST'>
+							<select name ='tri'>
+								<option value ='chrono' selected='selected'>Ordre chronologique</option>
+								<option value='alpha'>Ordre alphabétique</option>
+							</select>
+							<input id='button' type='submit' name = 'conf'></button>
+							<input id='rech' type='search' placeholder='Entrez votre recherche' name='q' />
+						</form>
+					</div>
+					
+					<div id='ajoutCarte'>
+						<a href='ajoutCartes.php'>Ajouter une carte</a>
+					</div>
+					
+					<div id='supprCarte'>
+						<a href='ajoutCartes.html'>Supprimer une/des carte(s)</a>
+					</div>
+			
+					<nav id='cartes'>
+						$strSearch
+					</nav>
+				</div>
+			</div>
+	</body>
+</html>";
+						
+					}
+				
+				}
+
+				if(!$_POST['q'] and !isset($_POST['conf']) ){
 				echo"<!DOCTYPE html>
 	<html lang='fr'>
 		<head>
@@ -329,6 +423,7 @@ if(isset($_SESSION['pseudo'])) {
 				</div>
 			</div>
 	</body>
-</html>";}
+</html>";
+	}
 }
 ?>
