7615ee6a24b8a5d5f8ed5a8a973b4c2a21988420
fabidesca <fabiandescampe@hotmail.com>
2018-12-16 18:30:42 +0100

    Modification carte de visite
    Modification carte de visite

--- Site/index.php
+++ Site/index.php
@@ -58,7 +58,7 @@ echo"<!DOCTYPE html>
 			</nav>
 			
 			<nav id='annonce'>
-				<a href= http://www.smartscan-bc.ovh\SitesPremium\Hydrogen\app\index.html ><img class='card_1' src='CSS/IMG/bc hydrogen.png' alt='BusinessCard'></a>
+				<a href= http://www.smartscan-bc.ovh\SitesPremium\Hydrogen\app\index.html ><img class='card_1' src='CSS/IMG/18.jpg' alt='BusinessCard'></a>
 				<img class='card_1' src='CSS/IMG/2.jpg' alt='BusinessCard'>
 				<img class='card_1' src='CSS/IMG/3.png' alt='BusinessCard'>
 				<img class='card_1' src='CSS/IMG/4.jpg' alt='BusinessCard'>
